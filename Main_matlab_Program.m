6232b4e5caee18fc3a8dd35cc20e136eaf1e6e23
Apoorv Kansal AK <apoorvkansalak@gmail.com>
2017-11-28 01:31:29 +0530

    Add files via upload

---
+++ Main_matlab_Program.m
@@ -0,0 +1,24 @@
+%A MATLAB file to implement all the other MATLAB functions and finally performing the real work.
+%This file is part of project IMGRG.
+%For more details see [log.txt]
+
+clear all
+close all
+clc
+fprintf('Loading the data ...\n')
+data = load('IMGRG_data.txt');
+X = data(:,1:784);
+y = data(:,785);
+num_labels = 5;
+input_layer_size = 784;
+m = size(X,1);
+fprintf('\nTraining One-vs-All Logistic Regression...\n')
+lambda = 0.1;
+[all_theta] = oneVsAll(X, y, num_labels, lambda);
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+testdata = load('IMGRG_testdata.txt');
+test_X = data(:,1:784);
+test_y = data(:,785);
+pred = predictOneVsAll(all_theta, test_X);
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == test_y)) * 100);
---
+++ Testing_Data_Creator.py
@@ -0,0 +1,41 @@
+""""
+This file is part of the project IMGRG.
+PS check log.txt for more details.
+
+A python 3.6 program to just gather the pixels of the Grayscale Images and convert them to pixels and store them in [IMGRG_testdata.txt].
+"""
+# All the files to be imported:
+from os import walk
+from PIL import Image
+
+def write_data(file_names,y,file_path):
+    """"
+    This is the most important function of this file.
+    Inputs : Accepts the {file_name} which is the list of the names of the files in a particular folder.
+           : Accepts the {y} which tells what is the value of the digit. y E {1,2,3,4,5}.
+           : Accepts the {file_path} which tells where the image is located.
+    Output : Void.
+    """
+    for file in file_names:
+        temp_mypath = file_path+'\\'+file
+        im = Image.open(temp_mypath)
+        pixels = list(im.getdata())
+        fh = open('IMGRG_testdata.txt','a')
+        for t in pixels:
+            fh.write(str(t))
+            fh.write(',')
+        fh.write(str(i))
+        fh.write('\n')
+        fh.close()
+
+
+
+mypath = r'E:\Codes\AI\My Projects\Image Recognition\Final\Testing data'
+
+for i in range(1,6):
+    temp_file_names = []
+    temp_mypath = mypath + '\\'+ str(i)
+    for (dirpath, dirnames, filenames) in walk(temp_mypath):
+        temp_file_names.extend(filenames)
+    write_data(temp_file_names,i,temp_mypath)
+print("Data has been created in the file [IMGRG_testdata.txt]")
