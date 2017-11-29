83a83cd2d388d3f4de07512014042956fe3d1c20
Apoorv Kansal AK <apoorvkansalak@gmail.com>
2017-11-29 21:20:24 +0530

    Image_Recognition_v1.2
    The documentation has been improved.
    Python files have become more responsive and informative to the console, with displaying the digit number while creating its data.
    The model size has increased and now accomodate all the digits and given digits, except {0}. This would be provided in a later update.
    The model has become less accurate the accuracy dropped from 98.72% to 94.82%, owing to the increased size of training data.
    The model is now saved as 100 iterations as default. This was the demand of the increasing size of data and decreasing accuracy rate.

--- Final_Program.py
+++ Final_Program.py
@@ -12,7 +12,7 @@ def write_data(file_names,y,file_path):
     """"
     This is the most important function of this file.
     Inputs : Accepts the {file_name} which is the list of the names of the files in a particular folder.
-           : Accepts the {y} which tells what is the value of the digit. y E {1,2,3,4,5}.
+           : Accepts the {y} which tells what is the value of the digit. y E {1,2,3,4,5,6,7,8,9}.
            : Accepts the {file_path} which tells where the image is located.
     Output : Void.
     """
@@ -32,12 +32,13 @@ def write_data(file_names,y,file_path):
 
 mypath = r'E:\Codes\AI\My Projects\Image Recognition\Final\Training_Set'
 
-for i in range(1,6):
+for i in range(1,10):
+    print("Creating data of digit",i)
     temp_file_names = []
     temp_mypath = mypath + '\\'+ str(i)
     for (dirpath, dirnames, filenames) in walk(temp_mypath):
         temp_file_names.extend(filenames)
     write_data(temp_file_names,i,temp_mypath)
-print("Data has been created in the file [IMGRG.txt]")
+print("Data has been created in the file [IMGRG_data.txt]")
 
 
